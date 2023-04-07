f2d5912b79860d7bbdc905c0ba3be4a66a9b1f92
sujinyang00 <sujinyang00@gmail.com>
2023-04-07 15:57:47 +0900

    dev 브랜치 pull 완료

--- src/NewApp.js
+++ src/NewApp.js
@@ -1,12 +1,4 @@
 import {StyleSheet, Text, View, Button, Alert} from 'react-native';
-<<<<<<<<< Temporary merge branch 1
-import React,{ useState, useEffect } from 'react';
-import Login from './Login';
-import Main1 from './Main1';
-import LogoTitle from "./component/LogoTitle";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-=========
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -16,23 +8,12 @@ import KakaoLogin from './screens/LoginScreen/KakaoLogin';
 import Main from './screens/Main';
 import LogoTitle from './screens/LogoTitle';
 import MainPage from './screens/MainPage';
->>>>>>>>> Temporary merge branch 2
 
 const Stack = createStackNavigator();
 export default function App() {
 
     const Stack = createStackNavigator();
     return (
-<<<<<<<<< Temporary merge branch 1
-        <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main1">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Main1" component={Main1} options={{headerTitle: props => <LogoTitle {...props} />}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-    );
-};
-=========
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Main">
           <Stack.Screen name="Main" component={Main} options={{headerTitle: props => <LogoTitle {...props} />}} />
@@ -42,4 +23,5 @@ export default function App() {
       </NavigationContainer>
     );
 }
->>>>>>>>> Temporary merge branch 2
+
+const styles = StyleSheet.create({});
