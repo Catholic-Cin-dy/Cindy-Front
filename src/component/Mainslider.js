68960e7902c219cddeb3e54b940ef5eabc5bfa76
yeonsu <kimys9295@naver.com>
2023-04-07 13:10:50 +0900

    메인 왼성

--- src/component/Mainslider.js
+++ src/component/Mainslider.js
@@ -1,50 +1,77 @@
 import React, { useState, useEffect }  from 'react';
 import {StyleSheet, Text, View, Button, Alert,Image,SafeAreaView,ScrollView} from 'react-native';
-import Swiper from 'react-native-web-swiper';
+import Swiper from 'react-native-swiper'
 import axios from 'axios';
 const baseUrl = 'https://www.awesominki.shop';
-export default class extends React.Component {
+
+export default function Mainslider() {
+    const [data, setData] = useState([]);
+    const [users, setUsers] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+    const config = {
+        headers: { 'X-AUTH-TOKEN': `eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOjIsImlhdCI6MTY3OTkyMjIwNSwiZXhwIjoxNzExNDU4MjA1fQ.A45bXqITjpGnywheSkEzfv5St2jD08DefUW2VQEbDpo` }
+    };
+
+    axios.get(baseUrl + '/home/banner', {...config })
+    .then(response =>setData(response.data.result))
+    .catch(error => console.error(error))
+    }, []);
 
 
-    render() {
         return (
-            <View style={styles.container}>
-                <Swiper>
-                    <View style={[styles.slideContainer,styles.slide2]} source={{
-                        uri: 'https://reactnative.dev/img/tiny_logo.png',
-                    }} >
-                        <Text>치삼이</Text>
-                        <Text></Text>
-                    </View>
-                    <View style={[styles.slideContainer,styles.slide2]}>
-                        <Text>Slide 2</Text>
-                    </View>
-                    <View style={[styles.slideContainer,styles.slide3]}>
-                        <Text>Slide 3</Text>
-                    </View>
+            <View style={styles.container1}>
+                <Swiper loop
+                        timeout={2}
+                        controlsEnabled={false}>
+                    {data.map(item => (
+                        <View style={styles.slide} key={item.id} >
+                            <Image style={styles.image} source={{ uri: item.bannerUrl}}/>
+                        </View>
+                    ))}
+
                 </Swiper>
             </View>
         );
-    }
+
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    container1: {
+        width:'100%',
         height: 519,
     },
-    slideContainer: {
+    wrapper: {
+
+    },
+    slide: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
-    slide1: {
-        backgraoundColor: 'rgba(200,20,20,0.3)',
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
     },
-    slide2: {
-        backgroundColor: 'rgba(20,200,20,0.3)',
-    },
-    slide3: {
-        backgroundColor: 'rgba(200,20,20,0.3)',
-    },
+
 });
+
+//<View style={[styles.slideContainer,styles.slide2]}>
+ //   <Text>Sdsklmd</Text>
+//</View>
+//<View style={[styles.slideContainer,styles.slide3]}>
+  //  <Text>Slide 3</Text>
+//</View>
+
+//slide1: {
+   // backgraoundColor: 'rgba(200,20,20,0.3)',
+//},
+//slide2: {
+   // backgroundColor: 'rgba(20,200,20,0.3)',
+//},
+//slide3: {
+ //   backgroundColor: 'rgba(200,20,20,0.3)',
+//},
