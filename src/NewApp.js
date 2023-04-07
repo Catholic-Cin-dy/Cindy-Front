6f2edf4bc2229e6f9938022f8d71d6fc985a119c
yeonsu <kimys9295@naver.com>
2023-04-07 14:20:04 +0900

    conflict 해결

--- src/NewApp.js
+++ src/NewApp.js
@@ -1,31 +1,14 @@
 import {StyleSheet, Text, View, Button, Alert} from 'react-native';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 
-import KakaoLogin from './screens/LoginScreen/KakaoLogin';
-import Main from './screens/Main';
-import LogoTitle from './screens/LogoTitle';
-import MainPage from './screens/MainPage';
-import React,{ useState, useEffect } from 'react';
 import Login from './Login';
-import Main1 from './Main1';
-import LogoTitle from "./component/LogoTitle";
 
-const Stack = createStackNavigator();
 export default function App() {
 
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen name="Main" component={Main} options={{headerTitle: props => <LogoTitle {...props} />}} />
-          <Stack.Screen name="KakaoLogin" component={KakaoLogin} />
-          <Stack.Screen name="MainPage" component={MainPage}/>
-            <Stack.Screen name="Main1" component={Main1} options={{headerTitle: props => <LogoTitle {...props} />}}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+        <View>
+            <Login/>
+
+        </View>
     );
-};
-
-
+}
--- src/component/Mainslider.js
+++ src/component/Mainslider.js
@@ -1,77 +1,50 @@
 import React, { useState, useEffect }  from 'react';
 import {StyleSheet, Text, View, Button, Alert,Image,SafeAreaView,ScrollView} from 'react-native';
-import Swiper from 'react-native-swiper'
+import Swiper from 'react-native-web-swiper';
 import axios from 'axios';
 const baseUrl = 'https://www.awesominki.shop';
-
-export default function Mainslider() {
-    const [data, setData] = useState([]);
-    const [users, setUsers] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-    const config = {
-        headers: { 'X-AUTH-TOKEN': `eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOjIsImlhdCI6MTY3OTkyMjIwNSwiZXhwIjoxNzExNDU4MjA1fQ.A45bXqITjpGnywheSkEzfv5St2jD08DefUW2VQEbDpo` }
-    };
-
-    axios.get(baseUrl + '/home/banner', {...config })
-    .then(response =>setData(response.data.result))
-    .catch(error => console.error(error))
-    }, []);
+export default class extends React.Component {
 
 
+    render() {
         return (
-            <View style={styles.container1}>
-                <Swiper loop
-                        timeout={2}
-                        controlsEnabled={false}>
-                    {data.map(item => (
-                        <View style={styles.slide} key={item.id} >
-                            <Image style={styles.image} source={{ uri: item.bannerUrl}}/>
-                        </View>
-                    ))}
-
+            <View style={styles.container}>
+                <Swiper>
+                    <View style={[styles.slideContainer,styles.slide2]} source={{
+                        uri: 'https://reactnative.dev/img/tiny_logo.png',
+                    }} >
+                        <Text>치삼이</Text>
+                        <Text></Text>
+                    </View>
+                    <View style={[styles.slideContainer,styles.slide2]}>
+                        <Text>Slide 2</Text>
+                    </View>
+                    <View style={[styles.slideContainer,styles.slide3]}>
+                        <Text>Slide 3</Text>
+                    </View>
                 </Swiper>
             </View>
         );
-
+    }
 }
 
 const styles = StyleSheet.create({
-    container1: {
-        width:'100%',
+    container: {
+        flex: 1,
         height: 519,
     },
-    wrapper: {
-
-    },
-    slide: {
+    slideContainer: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
-    image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
+    slide1: {
+        backgraoundColor: 'rgba(200,20,20,0.3)',
     },
-
+    slide2: {
+        backgroundColor: 'rgba(20,200,20,0.3)',
+    },
+    slide3: {
+        backgroundColor: 'rgba(200,20,20,0.3)',
+    },
 });
-
-//<View style={[styles.slideContainer,styles.slide2]}>
- //   <Text>Sdsklmd</Text>
-//</View>
-//<View style={[styles.slideContainer,styles.slide3]}>
-  //  <Text>Slide 3</Text>
-//</View>
-
-//slide1: {
-   // backgraoundColor: 'rgba(200,20,20,0.3)',
-//},
-//slide2: {
-   // backgroundColor: 'rgba(20,200,20,0.3)',
-//},
-//slide3: {
- //   backgroundColor: 'rgba(200,20,20,0.3)',
-//},
