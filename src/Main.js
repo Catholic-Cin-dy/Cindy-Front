e9938d2675a81552bf83eb25dd710b5c7186f281
onejuneee <onejuneee721@gmail.com>
2023-04-09 23:56:41 +0900

    Fix: 화면 전환 에러 수정중

--- src/Main.js
+++ src/Main.js
@@ -22,85 +22,63 @@ import {
 } from '@react-native-seoul/kakao-login';
 import {KakaoAccessTokenInfo} from '@react-native-seoul/kakao-login/src/types';
 
-export default function Main({navigation}) {
-  // const [categoryList, setCategoryList] = useState([]);
-  // const [gender, setGender] = useState('');
-  // const [name, setName] = useState('');
-  // const [nickname, setNickname] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [profileImgUrl, setProfileImgUrl] = useState('');
-  // const [socialId, setSocialId] = useState('');
-
-  // const handleSignUp = async (): Promise<void> => {
-  //   try {
-  //     // 회원가입 요청 데이터
-  //     const signUpData = {
-  //       categoryList: categoryList,
-  //       gender: gender,
-  //       name: name,
-  //       nickname: nickname,
-  //       password: password,
-  //       profileImgUrl: profileImgUrl,
-  //       socialId: socialId,
-  //     };
-  //
-  //     // 회원가입 API 호출
-  //     const response = await axios.post(
-  //       'https://www.awesominki.shop/auth/signup/kakao',
-  //       signUpData,
-  //     );
-  //
-  //     console.log(response);
-  //
-  //     // 회원가입이 성공하면 로그인 페이지로 이동합니다.
-  //     navigation.navigate('SignIn');
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+export default function Main({ navigation }) {
   const signInWithKakao = async (): Promise<void> => {
     try {
       const token: KakaoOAuthToken = await login();
       const accessToken = token.accessToken;
-      const response = await axios.post(
+      const userInfoResponse = await axios.get(
         'https://www.awesominki.shop/auth/kakao',
-        {accessToken},
+        { headers: { Authorization: `Bearer ${accessToken}` } },
       );
-      console.log(response);
-      //실험용 화면 전환
-    //   const userInfoResponse = await axios.get(
-    //     'https://www.awesominki.shop/auth/kakao',
-    //     { headers: { Authorization: `Bearer ${accessToken}` } },
-    //   );
-    //
-    //   // 카카오 사용자 정보 파싱
-    //   const userInfo = userInfoResponse.data;
-    //   const userId = userInfo.id.toString();
-    //   const nickname = userInfo.properties.nickname;
-    //
-    //   // 서버에서 회원 정보 요청
-    //   const userResponse = await axios.get(`https://www.awesominki.shop/user/${userId}`);
-    //
-    //   // 서버에 회원 정보가 등록되어 있지 않은 경우
-    //   if (userResponse.status === 404) {
-    //     // 회원가입 페이지로 이동
-    //     navigation.navigate('SignUp', { nickname });
-    //   } else {
-    //     // 회원 정보가 등록되어 있는 경우, 토큰 발급 요청
-    //     const signInData = {
-    //       userId: userId,
-    //       accessToken: accessToken,
-    //     };
-    //     const signInResponse = await axios.post('https://www.awesominki.shop/auth/signup/kakao', signInData);
-    //
-    //     // 토큰을 저장하고, 다음 페이지로 이동
-    //     const token = signInResponse.data.token;
-    //     // 다음 페이지로 이동하는 코드...
-    //   }
-    } catch (err) {
-      console.log(err);
+
+      // 카카오 사용자 정보 파싱
+      const userInfo = userInfoResponse.data;
+      const userId = userInfo.id.toString();
+      const nickname = userInfo.properties.nickname;
+
+      // 서버에서 회원 정보 요청
+      const userResponse = await axios.get(`https://www.awesominki.shop/user/${userId}`);
+
+      // 서버에 회원 정보가 등록되어 있지 않은 경우
+      if (userResponse.status === 404) {
+        // 회원가입 페이지로 이동
+        navigation.navigate('Test', {
+          categoryList: [],
+          gender: '',
+          name: '',
+          nickname: nickname,
+          password: '',
+          profileImgUrl: '',
+          socialId: userId,
+          isRequired: true
+        });
+      } else {
+        // 회원 정보가 등록되어 있는 경우, 토큰 발급 요청
+        const signInData = {
+          categoryList: [],
+          gender: '',
+          name: '',
+          nickname: nickname,
+          password: '',
+          profileImgUrl: '',
+          socialId: userId,
+        };
+        const signInResponse = await axios.post('https://www.awesominki.shop/auth/signup/kakao', signInData, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        // 토큰을 저장하고, 다음 페이지로 이동
+        const token = signInResponse.data.token;
+        // 다음 페이지로 이동하는 코드...
+      }
+    } catch (error) {
+      // 에러 처리 코드
     }
   };
+
 
   // const signInWithKakao = async (): Promise<void> => {
   //   const token: KakaoOAuthToken = await login();
@@ -131,23 +109,23 @@ export default function Main({navigation}) {
   //   // setResult(JSON.stringify(token));
   // };
 
-  const signOutWithKakao = async (): Promise<void> => {
-    const message = await logout();
-
-    setResult(message);
-  };
-
-  const getKakaoProfile = async (): Promise<void> => {
-    const profile: KakaoProfile = await getProfile();
-
-    setResult(JSON.stringify(profile));
-  };
-
-  const unlinkKakao = async (): Promise<void> => {
-    const message = await unlink();
-
-    setResult(message);
-  };
+  // const signOutWithKakao = async (): Promise<void> => {
+  //   const message = await logout();
+  //
+  //   setResult(message);
+  // };
+  //
+  // const getKakaoProfile = async (): Promise<void> => {
+  //   const profile: KakaoProfile = await getProfile();
+  //
+  //   setResult(JSON.stringify(profile));
+  // };
+  //
+  // const unlinkKakao = async (): Promise<void> => {
+  //   const message = await unlink();
+  //
+  //   setResult(message);
+  // };
 
   return (
     <View style={styles.container}>
--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   Alert,
@@ -9,41 +9,129 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
+export default function Test({navigation, route}) {
+  const [nickname, setNickname] = useState('');
 
-export default class Test extends Component {
-  state = {
-    text: '',
-    inputText: '',
+  const handleSubmit = () => {
+    navigation.navigate('Test2', {
+      categoryList: route.params.categoryList,
+      gender: route.params.gender,
+      name: route.params.name,
+      nickname,
+      password: route.params.password,
+      profileImgUrl: route.params.profileImgUrl,
+      socialId: route.params.socialId,
+      isRequired: route.params.isRequired,
+    });
   };
-  submitBtn = () => {
 
-    this.setState({text: this.state.inputText});
-  };
-  submitBtn = () => {
-    this.setState({text: this.state.inputText});
-  };
-
-  render() {
-    return (
-      <View style={styles.default}>
-        <Text style={styles.header3}>프로필을 설정해주세요</Text>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={text => {
-            this.setState({inputText: text});
-          }}
-          placeholder="닉네임 입력"
-        />
-        <TouchableOpacity style={styles.button} onPress={this.submitBtn}>
-          <Text style={styles.buttonText}>다음</Text>
-        </TouchableOpacity>
-        <Text style={styles.showText}>{this.state.text}</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>프로필을 설정해주세요</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => {
+          setNickname(text);
+        }}
+        placeholder="닉네임 입력"
+        value={nickname}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>다음</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 30,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 50,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#aaa',
+    borderRadius: 5,
+    padding: 10,
+    width: '100%',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#1abc9c',
+    borderRadius: 5,
+    padding: 15,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+
+
+
+
+
+// import React, {Component} from 'react';
+// import {
+//   Button,
+//   Alert,
+//   View,
+//   Text,
+//   StyleSheet,
+//   TextInput,
+//   TouchableOpacity,
+// } from 'react-native';
+//
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {NavigationContainer} from '@react-navigation/native';
+//
+// export default class Test extends Component {
+//   state = {
+//     text: '',
+//     inputText: '',
+//   };
+//   submitBtn = () => {
+//
+//     this.setState({text: this.state.inputText});
+//   };
+//   submitBtn = () => {
+//     this.setState({text: this.state.inputText});
+//   };
+//
+//   render() {
+//     return (
+//       <View style={styles.default}>
+//         <Text style={styles.header3}>프로필을 설정해주세요</Text>
+//         <TextInput
+//           style={styles.textInput}
+//           onChangeText={text => {
+//             this.setState({inputText: text});
+//           }}
+//           placeholder="닉네임 입력"
+//         />
+//         <TouchableOpacity style={styles.button} onPress={this.submitBtn}>
+//           <Text style={styles.buttonText}>다음</Text>
+//         </TouchableOpacity>
+//         <Text style={styles.showText}>{this.state.text}</Text>
+//       </View>
+//     );
+//   }
+// }
+
+
+
+
 // function Test() {
 //
 //     return (
@@ -59,46 +147,46 @@ export default class Test extends Component {
 //     );
 // }
 
-const styles = StyleSheet.create({
-  default: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingLeft: 16,
-    paddingRight: 16,
-    paddingTop: 16,
-  },
-  header3: {
-    textAlign: 'left',
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  textInput: {
-    marginTop: 80,
-    marginBottom: 32,
-    height: 48,
-    borderBottomWidth: 1,
-  },
-  showText: {
-    marginTop: 17,
-    fontSize: 16,
-    color: 'white',
-  },
-  btn_move: {
-    weight: 320,
-    height: 50,
-    backgroundColor: 'black',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  button: {
-    backgroundColor: 'black',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 4,
-  },
-});
+// const styles = StyleSheet.create({
+//   default: {
+//     flex: 1,
+//     backgroundColor: 'white',
+//     paddingLeft: 16,
+//     paddingRight: 16,
+//     paddingTop: 16,
+//   },
+//   header3: {
+//     textAlign: 'left',
+//     color: 'black',
+//     fontWeight: 'bold',
+//     fontSize: 20,
+//   },
+//   textInput: {
+//     marginTop: 80,
+//     marginBottom: 32,
+//     height: 48,
+//     borderBottomWidth: 1,
+//   },
+//   showText: {
+//     marginTop: 17,
+//     fontSize: 16,
+//     color: 'white',
+//   },
+//   btn_move: {
+//     weight: 320,
+//     height: 50,
+//     backgroundColor: 'black',
+//   },
+//   buttonText: {
+//     color: '#FFFFFF',
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//   },
+//   button: {
+//     backgroundColor: 'black',
+//     paddingVertical: 10,
+//     paddingHorizontal: 20,
+//     borderRadius: 4,
+//   },
+// });
