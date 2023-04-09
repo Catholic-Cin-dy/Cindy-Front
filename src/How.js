165c1cbceaa5e49b151602c7c0e8082a6486b42a
onejuneee <onejuneee721@gmail.com>
2023-04-10 03:39:36 +0900

    Fix: JWT 헤더 주소 삽입오류(미해결)

---
+++ src/How.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Main from './Main';
+
+export default function How() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
