f372b58d408da9fae3a9fad6d32a456c3941c8fe
onejuneee <onejuneee721@gmail.com>
2023-05-10 14:13:53 +0900

    Fix: 위도 경도 받기 수정

--- src/How.js
+++ src/How.js
@@ -2,10 +2,6 @@ import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import {check, PERMISSIONS, request} from 'react-native-permissions';
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
-import { check, PERMISSIONS, request } from 'react-native-permissions';
 
 export default function How() {
   const [position, setPosition] = useState(null);
@@ -22,10 +18,12 @@ export default function How() {
             error => {
               console.log(error.code, error.message);
             },
-            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+            {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
           );
         } else if (granted === 'denied') {
-          const requestResult = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+          const requestResult = await request(
+            PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+          );
           if (requestResult === 'granted') {
             Geolocation.getCurrentPosition(
               position => {
@@ -34,7 +32,7 @@ export default function How() {
               error => {
                 console.log(error.code, error.message);
               },
-              { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+              {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
             );
           } else {
             console.log('Location permission not granted.');
@@ -47,32 +45,6 @@ export default function How() {
 
     requestLocationPermission();
   }, []);
-
-  return (
-    <View style={styles.container}>
-      {position ? (
-        <>
-          <Text style={styles.text}>Latitude: {position.coords.latitude}</Text>
-          <Text style={styles.text}>Longitude: {position.coords.longitude}</Text>
-        </>
-      ) : (
-        <Text style={styles.text}>Waiting for location...</Text>
-      )}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
 
   return (
     <View style={styles.container}>
