2b8965877db38c73e82ebffcaedcb860770a92cf
yeonsu <kimys9295@naver.com>
2023-06-02 11:02:32 +0900

    fix post 진행중

--- src/screens/Community/CommWrite.js
+++ src/screens/Community/CommWrite.js
@@ -20,6 +20,7 @@ export default function CommWrite() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [img, setImageSource] = useState('');
+  const [imgurl, setImgurl] = useState([]);
 
   const onChangeTitle = inputText => {
     setTitle(inputText);
@@ -35,12 +36,30 @@ export default function CommWrite() {
     maxHeight: 300,
     includeBase64: Platform.OS === 'android',
   };
-  const onPickImage = res => {
+  let imgUrlsource;
+
+  const onPickImage = async res => {
     if (res.didCancel || !res) {
       return;
     }
-    console.log('PickImage', res);
-    setImageSource(res.uri);
+    //console.log(res);
+    //console.log(res.uri);
+    console.log(res.assets[0].uri);
+
+    imgUrlsource = res.assets[0].uri;
+    const response = await fetch(imgUrlsource);
+    const blob = await response.blob();
+    setImageSource(res.assets[0].uri);
+    //submitBtn();
+    //submitBtn(); 비로 실행되게 만듦 이유 :imgUrlsource 변수에 값을 할당되기 전 먼저 함수 호출된다 해서 ㅅㅍ
+    const newImgList = [
+      {
+        uri: imgUrlsource,
+        type: 'image/jpeg',
+        name: 'image.jpg',
+      },
+    ];
+    setImgurl(newImgList);
   };
 
   // 카메라 촬영
@@ -49,7 +68,7 @@ export default function CommWrite() {
   };
 
   // 갤러리에서 사진 선택
-  const onLaunchImageLibrary = () => {
+  const ShowPicker = () => {
     launchImageLibrary(imagePickerOption, onPickImage);
   };
 
@@ -65,36 +84,110 @@ export default function CommWrite() {
       // iOS
     }
   };
-  const imageUrls = [
-    'https://example.com/image1.jpeg',
-    'https://example.com/image2.jpeg',
-    'https://example.com/image3.jpeg',
-    // Add more URLs as needed
-  ];
+
   const submitBtn = () => {
     const formData = new FormData();
-    formData.append('content', JSON.stringify(content));
-    formData.append('title', JSON.stringify(title));
-    imageUrls.forEach((url, index) => {
-      formData.append(`image${index + 1}`, {
-        uri: url,
-        name: `image${index + 1}.jpg`,
-        type: 'image/jpeg',
-      });
+    //const postBoard = new FormData();
+    formData.append('imgUrl', imgurl[0], {
+      type: 'multipart/form-data',
     });
-    console.log('formData : ' + formData.title);
+    const postBoard = {
+      title: title,
+      content: content,
+      latitude: 11.12,
+      longitude: 23.45,
+      tags: [],
+      imgList: [
+        {
+          imgTags: [
+            {
+              brandId: 3,
+              x: 70,
+              y: 67,
+            },
+            {
+              brandId: 4,
+              x: 125,
+              y: 125,
+            },
+          ],
+        },
+        {
+          imgTags: [
+            {
+              brandId: 1,
+              x: 30,
+              y: 67,
+            },
+            {
+              brandId: 2,
+              x: 251,
+              y: 125,
+            },
+          ],
+        },
+      ],
+    };
+    // const postBoardBlob = new Blob([JSON.stringify(postBoard1)], {
+    //   type: 'application/json',
+    // });
+    //formData.append('postBoard', postBoardBlob);
+
+    formData.append('postBoard', JSON.stringify(postBoard), {
+      type: 'application/json',
+    });
+    // postBoard.append('title', JSON.stringify(postBoard1.title));
+    // postBoard.append('content', JSON.stringify(postBoard1.content));
+    // postBoard.append('latitude', JSON.stringify(postBoard1.latitude));
+    // postBoard.append('longitude', JSON.stringify(postBoard1.longitude));
+    // postBoard.append('tags', JSON.stringify(postBoard1.tags));
+    //
+    // postBoard1.imgList.forEach((imgObj, index) => {
+    //   imgObj.imgTags.forEach(imgTag => {
+    //     postBoard.append(
+    //       `imgList[${index}][imgTags][${index}]brandId`,
+    //       JSON.stringify(imgTag.brandId),
+    //     );
+    //     postBoard.append(
+    //       `imgList[${index}][imgTags][${index}]x`,
+    //       JSON.stringify(imgTag.x),
+    //     );
+    //     postBoard.append(
+    //       `imgList[${index}][imgTags][${index}]y`,
+    //       JSON.stringify(imgTag.y),
+    //     );
+    //   });
+    // });
+
+    //console.log('postBoard:', postBoard);
+
+    console.log('imgUrl:', imgurl);
+    //console.log('imgUrl폼:', Object(imgUrl));
+    console.log('postboard폼:', Object(postBoard));
+
     const authToken =
       'eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOjIsImlhdCI6MTY3OTkyMjIwNSwiZXhwIjoxNzExNDU4MjA1fQ.A45bXqITjpGnywheSkEzfv5St2jD08DefUW2VQEbDpo';
-    const requestOptions = {
+    const imgConfig = {
       method: 'POST',
       headers: {
         // multipart/
         'Content-Type': 'multipart/form-data',
         'X-AUTH-TOKEN': authToken,
       },
-      //body: formData,
+      body: formData,
     };
-    fetch(baseUrl + '/boards/new', requestOptions)
+    // console.log('바디' + body);
+    // const boardConfig = {
+    //   method: 'POST',
+    //   headers: {
+    //     // multipart/
+    //     'Content-Type': 'application/json',
+    //     'X-AUTH-TOKEN': authToken,
+    //   },
+    //   body: imgUrl,
+    // };
+
+    fetch(baseUrl + '/boards/new', imgConfig)
       .then(response => {
         // Handle the response as needed
         // For example, check the response status and do appropriate actions
@@ -104,16 +197,27 @@ export default function CommWrite() {
           console.log('Request failed');
         }
       })
+
       .catch(error => console.error(error));
+
+    //   fetch(baseUrl + '/boards/new', boardConfig)
+    //     .then(response => {
+    //       if (response.ok) {
+    //         console.log('Board creation successful');
+    //       } else {
+    //         console.log('Board creation failed');
+    //       }
+    //     })
+    //     .catch(error => console.error(error));
   };
 
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
-        <Text>이미지를 선택해주세요</Text>
+        <Text style={styles.text1}>이미지를 선택해주세요</Text>
         {img ? ( // 이미지가 있으면 라이브러리에서 받아온 이미지로 출력, 없으면 디폴트 이미지 출력!
           <Pressable style={styles.image} onPress={() => modalOpen()}>
-            <Image source={{uri: img}} style={{width: 300, height: 300}} />
+            <Image source={{uri: img}} style={{width: 380, height: 400}} />
           </Pressable>
         ) : (
           <Pressable style={styles.image} onPress={() => modalOpen()}>
@@ -124,19 +228,19 @@ export default function CommWrite() {
           visible={modalVisible}
           onClose={() => setModalVisible(false)}
           onLaunchCamera={onLaunchCamera}
-          onLaunchImageLibrary={onLaunchImageLibrary}
+          onLaunchImageLibrary={ShowPicker}
         />
         {/*모달 띄우기*/}
-        <Text>지도</Text>
+        <Text style={styles.text1}>지도</Text>
         <View style={styles.map} />
-        <Text>제목을 작성해주세요</Text>
+        <Text style={styles.text1}>제목을 작성해주세요</Text>
         <TextInput
-          style={styles.input}
+          style={styles.input1}
           onChangeText={onChangeTitle}
           value={title}
           placeholder="제목을 작성해주세요."
         />
-        <Text> 내용을 작성해주세요</Text>
+        <Text style={styles.text1}> 내용을 작성해주세요</Text>
         <TextInput
           style={styles.input}
           onChangeText={onChangeContent}
@@ -152,10 +256,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  text1: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    marginLeft: 10,
+  },
   image: {
-    marginLeft: 50,
-    width: 500,
-    height: 500,
+    marginLeft: 10,
+    width: 380,
+    height: 400,
     backgroundColor: 'gray',
   },
   map: {
@@ -168,9 +278,17 @@ const styles = StyleSheet.create({
   input: {
     marginTop: 40,
     marginLeft: 5,
-    marginBottom: 30,
+    marginBottom: 10,
     width: 400,
     height: 200,
+    backgroundColor: 'gray',
+  },
+  input1: {
+    marginTop: 40,
+    marginLeft: 5,
+    marginBottom: 30,
+    width: 400,
+    height: 80,
     backgroundColor: 'gray',
   },
 });
--- src/screens/Community/CommuScreen.js
+++ src/screens/Community/CommuScreen.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
-import { TouchableOpacity, FlatList, TextInput, View, StyleSheet, Text, Button,SafeAreaView,ScrollView } from 'react-native';
-import CommWrite from "./CommWrite";
+import React, {useState} from 'react';
+import {
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import CommWrite from './CommWrite';
 import axios from 'axios';
-import {useNavigation} from "@react-navigation/native";
+import {useNavigation} from '@react-navigation/native';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {PermissionsAndroid} from 'react-native';
 
 const baseUrl = 'https://www.awesominki.shop/'; //api 연결을 위한 baseUrl
 const config = {
-  headers: { 'X-AUTH-TOKEN': `eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOjIsImlhdCI6MTY3OTkyMjIwNSwiZXhwIjoxNzExNDU4MjA1fQ.A45bXqITjpGnywheSkEzfv5St2jD08DefUW2VQEbDpo` }
+  headers: {
+    'X-AUTH-TOKEN': `eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOjIsImlhdCI6MTY3OTkyMjIwNSwiZXhwIjoxNzExNDU4MjA1fQ.A45bXqITjpGnywheSkEzfv5St2jD08DefUW2VQEbDpo`,
+  },
 };
 export default function CommuScreen() {
   const navigation = useNavigation();
@@ -18,66 +30,63 @@ export default function CommuScreen() {
   const [searchText, setSearchText] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 
-  const handleSearchTextChange = (text) => {
+  const handleSearchTextChange = text => {
     setSearchText(text);
     if (text.length > 0) {
       // TODO: Implement your logic to get suggestions based on the input text
       //setSuggestions([...Array(5)].map((_, i) => ({ label: `${text} ${i + 1}` })));
       const params = {
-        content: text
+        content: text,
       };
 
-      axios.get(baseUrl + 'boards/tag', { params, ...config })
-        .then(response =>
-          setSuggestions(response.data.result)
-        )
-        .catch(error => console.error(error))
-
-    } else { //검색어 없으면 빈칸
+      axios
+        .get(baseUrl + 'boards/tag', {params, ...config})
+        .then(response => setSuggestions(response.data.result))
+        .catch(error => console.error(error));
+    } else {
+      //검색어 없으면 빈칸
       setSuggestions([]);
     }
   };
-  const handleSuggestionPress = (item) => {
+  const handleSuggestionPress = item => {
     setSearchText(item);
   };
 
-  const renderSuggestion = ({ item }) => {
+  const renderSuggestion = ({item}) => {
     return (
-      <TouchableOpacity
-        onPress={() => handleSuggestionPress(item)}
-      >
+      <TouchableOpacity onPress={() => handleSuggestionPress(item)}>
         <Text>{item}</Text>
       </TouchableOpacity>
     );
   };
 
   return (
-      <ScrollView>
-        <SafeAreaView style={styles.container}>
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
         <Text>커뮤니티 입니다</Text>
 
-          <TextInput
-            style={styles.input}
-            placeholder="검색어를 입력하세요."
-            onChangeText={handleSearchTextChange}
-            value={searchText}
-          />
-          <FlatList
-            data={suggestions}
-            renderItem={renderSuggestion}
-            keyExtractor={(item) => item}
-          />
+        <TextInput
+          style={styles.input}
+          placeholder="검색어를 입력하세요."
+          onChangeText={handleSearchTextChange}
+          value={searchText}
+        />
+        <FlatList
+          data={suggestions}
+          renderItem={renderSuggestion}
+          keyExtractor={item => item}
+        />
 
-          <Text>검색어 : {searchText}</Text>
+        <Text>검색어 : {searchText}</Text>
 
-
-          <Button title={"글쓰기"} onPress={() => {
-            navigation.navigate("CommWrite");
-          }} />
-        </SafeAreaView>
-
-      </ScrollView>
-
+        <Button
+          title={'글쓰기'}
+          onPress={() => {
+            navigation.navigate('CommWrite');
+          }}
+        />
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
@@ -95,7 +104,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FDF5DC',
     paddingHorizontal: 20,
     marginVertical: 30,
-    flex: 1
+    flex: 1,
   },
   textInput: {
     marginTop: 20,
@@ -104,27 +113,27 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 10,
     borderColor: 'gray',
-    borderWidth: 1
+    borderWidth: 1,
   },
   showText: {
     marginTop: 10,
     fontSize: 25,
   },
 
-  Product : {
+  Product: {
     width: 156,
     height: 168,
     borderRadius: 8,
   },
 
-  SearchContainer : {
+  SearchContainer: {
     width: 400,
     height: 45,
     // position: relative,
     border: 0,
   },
 
-  Search : {
+  Search: {
     border: 0,
     backgroundColor: '#eaeaea',
     paddingLeft: 10,
