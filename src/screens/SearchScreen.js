abc85a334c22df944029283e88c15b982957eb3d
sujinyang00 <sujinyang00@gmail.com>
2023-06-06 02:27:23 +0900

    여깄습니다. 기원준ㅆ;

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -32,22 +32,89 @@ const config = {
 
 export default function SearchScreen() {
 
+  const [text, setText] = useState('');
+
+  const [searchText, setSearchText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
 
 
   },);
 
+  const handleSearchTextChange = text => {
+    setSearchText(text);
+    if (text.length > 0) {
+      // TODO: Implement your logic to get suggestions based on the input text
+      //setSuggestions([...Array(5)].map((_, i) => ({ label: `${text} ${i + 1}` })));
+      const params = {
+        page: 0,
+        content: text,
+      };
+
+
+      axios
+        .get(baseUrl + 'boards/tag', {params, ...config})
+        .then(response => setSuggestions(response.data.result))
+        .catch(error => console.error(error));
+
+
+
+    } else {
+      //검색어 없으면 빈칸
+      setSuggestions([]);
+    }
+  };
+  const handleSuggestionPress = item => {
+    setSearchText(item);
+  };
+
+  const renderSuggestion = ({item}) => {
+    return (
+      <TouchableOpacity onPress={() => handleSuggestionPress(item)}>
+        <Text>{item}</Text>
+      </TouchableOpacity>
+    );
+  };
+
 
   return (
     <View style={styles.firstView}>
       <Text>검색화면</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요."
+        onChangeText={handleSearchTextChange}
+        value={searchText}
+      />
+      <FlatList
+        data = {suggestions}
+        renderItem = {renderSuggestion}
+        keyExtractor = {item => item}>
+      </FlatList>
+
+      <Text>검색어 : {searchText}</Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   firstView: {
     backgroundColor: 'white',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  textInput: {
+    marginTop: 20,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    height: 40,
+    borderRadius: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 
 });
