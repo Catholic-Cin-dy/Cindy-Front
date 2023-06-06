8410fdaea10075cf34155bedc875b2e015e7039c
onejuneee <onejuneee721@gmail.com>
2023-06-06 17:53:11 +0900

    Feat: 댓글 뷰 스타일 수정

--- src/screens/Community/CommuPostDetail.js
+++ src/screens/Community/CommuPostDetail.js
@@ -11,9 +11,12 @@ import {
   TextInput,
   Modal,
   ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
 } from 'react-native';
 //import Modal from 'react-native-simple-modal';
-import { MenuProvider } from 'react-native-popup-menu';
+import {MenuProvider} from 'react-native-popup-menu';
 //import Modal from 'react-native-simple-modal';
 import Swiper from 'react-native-web-swiper';
 import axios from 'axios';
@@ -28,6 +31,8 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useIsFocused} from '@react-navigation/native';
 
 import CommuScreen from './CommuScreen';
+import {Keyboard} from 'swiper';
+// import {Keyboard} from 'swiper';
 
 const baseUrl = 'https://www.awesominki.shop/'; //api 연결을 위한 baseUrl
 const config = {
@@ -75,7 +80,7 @@ export default function CommuPostDetail({route}) {
     if (textRef.current) {
       textRef.current.measure((x, y, width, height) => {
         setTextWidth(width);
-        console.log('글자길이 : '+textWidth);
+        console.log('글자길이 : ' + textWidth);
       });
     }
 
@@ -191,12 +196,11 @@ export default function CommuPostDetail({route}) {
 
     axios
       .delete(baseUrl + 'boards/delete/' + boardId, {...config})
-      .then(response => {
-
-      })
+      .then(response => {})
       .catch(error => console.error(error));
   }
-  function modifyPost() { //수정 api바뀌면 적용 예정
+  function modifyPost() {
+    //수정 api바뀌면 적용 예정
     const params = {
       boardId: boardId,
     };
@@ -208,7 +212,6 @@ export default function CommuPostDetail({route}) {
       })
       .catch(error => console.error(error));*/
   }
-
 
   function movePost() {
     navigation.pop();
@@ -229,12 +232,12 @@ export default function CommuPostDetail({route}) {
             {data.profileImgUrl ? (
               <Image
                 style={styles.profileImg}
-                source={{ uri: data.profileImgUrl }}
+                source={{uri: data.profileImgUrl}}
               />
             ) : (
               <Image
                 style={styles.defaultImg}
-                source={require("../../assets/user.png")}
+                source={require('../../assets/user.png')}
               />
             )}
             <Text style={styles.info2}>{data.writer}</Text>
@@ -255,7 +258,7 @@ export default function CommuPostDetail({route}) {
                   <Text>삭제</Text>
                 </TouchableOpacity>
               ) : (
-                "유효값x pid: " + data.boardId
+                '유효값x pid: ' + data.boardId
               )}
             </Text>
 
@@ -269,40 +272,52 @@ export default function CommuPostDetail({route}) {
                   <Text>수정</Text>
                 </TouchableOpacity>
               ) : (
-                "유효값x pid: " + data.boardId
+                '유효값x pid: ' + data.boardId
               )}
             </Text>
           </View>
 
-
           <View style={styles.container}>
             <View style={styles.slideImageView}>
-              <Swiper
-                containerStyle={{width:360, height:400}}>
+              <Swiper containerStyle={{width: 360, height: 400}}>
                 {data &&
                   data.imgList &&
                   data.imgList.map((img, index) => (
-                    <TouchableOpacity onPress={showCoordinate} style={styles.postImgWhole}>
+                    <TouchableOpacity
+                      onPress={showCoordinate}
+                      style={styles.postImgWhole}>
                       <ImageBackground
                         key={index}
-                        source={{ uri: img.imgUrl }}
-                        style={styles.pImg}
-                      >
+                        source={{uri: img.imgUrl}}
+                        style={styles.pImg}>
                         {img.imgTags &&
                           img.imgTags.map((tag, index) => (
                             <View
                               key={index}
-                              style={[styles.imgtagContainer, { width: 360, height: 400, position: "relative" }]}
-                            >
-                              <View style={[styles.imgTagBox, { position: 'absolute', left: tag.x, top: tag.y - index * 400 }]}>
-                                <Text style={[styles.imgTagText, { flexWrap: 'wrap' }]}>
+                              style={[
+                                styles.imgtagContainer,
+                                {width: 360, height: 400, position: 'relative'},
+                              ]}>
+                              <View
+                                style={[
+                                  styles.imgTagBox,
+                                  {
+                                    position: 'absolute',
+                                    left: tag.x,
+                                    top: tag.y - index * 400,
+                                  },
+                                ]}>
+                                <Text
+                                  style={[
+                                    styles.imgTagText,
+                                    {flexWrap: 'wrap'},
+                                  ]}>
                                   {`${tag.brandName}`}
                                 </Text>
                               </View>
                             </View>
                           ))}
                       </ImageBackground>
-
 
                       {/*<View>
                       <Text>---------------------------------------------------------------------</Text>
@@ -323,7 +338,6 @@ export default function CommuPostDetail({route}) {
             </View>
           </View>
 
-
           <View style={styles.content} key={data.boardId}>
             <View style={styles.contentContainer}>
               <Text style={styles.info1}>{data.title}</Text>
@@ -331,7 +345,7 @@ export default function CommuPostDetail({route}) {
                 {/*<Text>{data.likeCheck ? data.likeCheck.toString() : "유효값x pid: " + data.boardId}</Text>*/}
 
                 <Text>
-                  {data.my ? data.my.toString() : "false" + data.boardId}
+                  {data.my ? data.my.toString() : 'false' + data.boardId}
                 </Text>
 
                 <TouchableOpacity onPress={handleLike}>
@@ -339,8 +353,8 @@ export default function CommuPostDetail({route}) {
                     style={styles.heartIcon}
                     source={
                       data.likeCheck
-                        ? require("../../assets/like.png")
-                        : require("../../assets/unlike.png")
+                        ? require('../../assets/like.png')
+                        : require('../../assets/unlike.png')
                     }
                   />
                 </TouchableOpacity>
@@ -358,12 +372,12 @@ export default function CommuPostDetail({route}) {
                 {item.profileImgUrl ? (
                   <Image
                     style={styles.profileImg}
-                    source={{ uri: item.profileImgUrl }}
+                    source={{uri: item.profileImgUrl}}
                   />
                 ) : (
                   <Image
                     style={styles.defaultImg}
-                    source={require("../../assets/user.png")}
+                    source={require('../../assets/user.png')}
                   />
                 )}
                 <Text style={styles.info2}>{item.nickName}</Text>
@@ -385,7 +399,7 @@ export default function CommuPostDetail({route}) {
                     //   />
                     //   <Button title="전송" onPress={writeComment} />
                     // </View>
-                    ""
+                    ''
                   ) : (
                     <TouchableOpacity
                       style={styles.cdeleteBtn1}
@@ -400,7 +414,7 @@ export default function CommuPostDetail({route}) {
                 </View>
               </View>
               {isEditing && CommentIdBeingEdited === item.commentId ? (
-                <View style={{ flexDirection: "row" }}>
+                <View style={{flexDirection: 'row'}}>
                   <TextInput
                     style={styles.fixinput1}
                     onChangeText={onChangeComment2}
@@ -416,7 +430,6 @@ export default function CommuPostDetail({route}) {
               )}
               <Text style={styles.info1}>작성일시 {item.commentTime}</Text>
               <Text style={styles.info2}>{item.my}</Text>
-
             </View>
           ))}
         </View>
@@ -424,19 +437,74 @@ export default function CommuPostDetail({route}) {
         <Text />
         <Text />
         <Text />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeComment}
-          value={comment}
-          placeholder="댓글을 입력해주세요."
-        />
-        <Button title="전송" onPress={writeComment} />
+
+        <KeyboardAvoidingView
+          style={{flex: 1}}
+          enabled
+          behavior={Platform.OS === 'ios' ? 'padding' : null}>
+          {/*이부분 수정 필요*/}
+          {/*<View style={styles.Contiainer}>*/}
+          {/*  <TouchableWithoutFeedback*/}
+          {/*    onPress={Keyboard.dismiss}*/}
+          {/*    style={{height: '100%'}}*/}
+          {/*  />*/}
+          {/*</View>*/}
+          <View style={styles.EditorContainer}>
+            <Image
+              style={{
+                height: 30,
+                width: 30,
+                borderRadius: 15,
+                marginRight: 10,
+                marginLeft: 10,
+                padding: 10,
+              }}
+              source={require('../../assets/user.png')}
+            />
+            <TextInput
+              style={{flex: 1}}
+              multiline
+              onChangeText={onChangeComment}
+              value={comment}
+              placeholder="댓글을 입력해주세요."
+              autoFocus={true}
+            />
+            <TouchableOpacity style={{width: 50}} onPress={writeComment}>
+              <Text style={styles.blueColor}>전송</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+
+        {/*<TextInput*/}
+        {/*  style={styles.input}*/}
+        {/*  onChangeText={onChangeComment}*/}
+        {/*  value={comment}*/}
+        {/*  placeholder="댓글을 입력해주세요."*/}
+        {/*/>*/}
+        {/*<Button title="전송" onPress={writeComment} />*/}
       </ScrollView>
     </View>
   );
 }
 
 const styles = {
+  blueColor: {
+    color: 'blue',
+  },
+  Contiainer: {
+    backgroundColor: 'transparent',
+    flex: 1,
+  },
+  EditorContainer: {
+    padding: 5,
+    minHeight: 65,
+    borderTopColor: 'grey',
+    borderTopWidth: 0.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
   postImgWhole: {
     /*flexDirection: 'row',*/
   },
